5e9104deb81352f5f021180550d0c19857875322
IDKiro <609568960@qq.com>
2018-03-22 10:50:15 +0800

    init

--- ssmgr_install.sh
+++ ssmgr_install.sh
@@ -60,7 +60,7 @@ get_latest_version(){
     [ -z ${ver} ] && echo "Error: Get shadowsocks-libev latest version failed" && exit 1
     shadowsocks_libev_ver="shadowsocks-libev-$(echo ${ver} | sed -e 's/^[a-zA-Z]//g')"
     download_link="https://github.com/shadowsocks/shadowsocks-libev/releases/download/${ver}/${shadowsocks_libev_ver}.tar.gz"
-    ss_init_script_link="https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-libev"
+    ss_init_script_link="https://raw.githubusercontent.com/IDKiro/ssmgr-install/master/shadowsocks"
 }
 
 pre_install(){
